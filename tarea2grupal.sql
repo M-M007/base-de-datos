1ff6f282b4c02f31c2e8f0030961189240afb337
Matias Montaño <72836339+M-M007@users.noreply.github.com>
2021-09-30 13:22:05 -0300

    Update tarea2grupal.sql

--- tarea2grupal.sql
+++ tarea2grupal.sql
@@ -1,27 +1,18 @@
 --Tarea para crea
 --2.3. Ejercicio 1: Definición de Datos (Actividad Grupal)
+--La siguiente base de datos está pensada para almacenar la información necesaria para gestionar la venta automática de entradas para diferentes espectáculos desde múltiples puntos de venta, como pueden ser oficinas bancarias, terminales tipo Servicaixa, o las mismas taquillas de teatros u otros recintos.
 
-ESPECTACULOS (COD_ESPECTACULO, NOMBRE, TIPO, FECHA_INICIAL,
-FECHA_FINAL, INTERPRETE, COD_RECINTO)
+--ESPECTACULOS (COD_ESPECTACULO, NOMBRE, TIPO, FECHA_INICIAL,FECHA_FINAL, INTERPRETE, COD_RECINTO)
+--PRECIOS_ESPECTACULOS (COD_ESPECTACULO, COD_RECINTO, ZONA,PRECIO)
+--RECINTOS (COD_RECINTO, NOMBRE, DIRECCION, CIUDAD, TELEFONO,HORARIO)
+--ZONAS_RECINTOS (COD_RECINTO, ZONA, CAPACIDAD)
+--ASIENTOS (COD_RECINTO, ZONA, FILA, NUMERO)
+--REPRESENTACIONES (COD_ESPECTACULO, FECHA, HORA)
+--ENTRADAS (COD_ESPECTACULO, FECHA, HORA, COD_RECINTO, FILA,NUMERO, ZONA, DNI_CLIENTE)
+--ESPECTADORES (DNI_CLIENTE, NOMBRE, DIRECCION, TELEFONO, CIUDAD,NTARJETA)
 
-PRECIOS_ESPECTACULOS (COD_ESPECTACULO, COD_RECINTO, ZONA,
-PRECIO)
+/* base de datos es */
 
-RECINTOS (COD_RECINTO, NOMBRE, DIRECCION, CIUDAD, TELEFONO,
-HORARIO)
-
-ZONAS_RECINTOS (COD_RECINTO, ZONA, CAPACIDAD)
-ASIENTOS (COD_RECINTO, ZONA, FILA, NUMERO)
-
-REPRESENTACIONES (COD_ESPECTACULO, FECHA, HORA)
-
-ENTRADAS (COD_ESPECTACULO, FECHA, HORA, COD_RECINTO, FILA,
-NUMERO, ZONA, DNI_CLIENTE)
-
-ESPECTADORES (DNI_CLIENTE, NOMBRE, DIRECCION, TELEFONO, CIUDAD,
-NTARJETA)
-
-stablecer las claves primarias de cada una de las tablas
 
 CREATE TABLE personas.dbo.city (
     id int  NOT NULL IDENTITY(1, 1),
@@ -31,3 +22,27 @@ CREATE TABLE personas.dbo.city (
     country_id int  NOT NULL,
     CONSTRAINT city_pk PRIMARY KEY  (id)
 );
+
+CREATE TABLE personas.dbo.city (
+    id int  NOT NULL IDENTITY(1, 1),
+    city_name char(128)  NOT NULL,
+    lat decimal(9,6)  NOT NULL,
+    long decimal(9,6)  NOT NULL,
+    country_id int  NOT NULL,
+    CONSTRAINT city_pk PRIMARY KEY  (id)
+);
+
+CREATE TABLE personas.dbo.city (
+    id int  NOT NULL IDENTITY(1, 1),
+    city_name char(128)  NOT NULL,
+    lat decimal(9,6)  NOT NULL,
+    long decimal(9,6)  NOT NULL,
+    country_id int  NOT NULL,
+    CONSTRAINT city_pk PRIMARY KEY  (id)
+);
+
+
+
+
+
+
